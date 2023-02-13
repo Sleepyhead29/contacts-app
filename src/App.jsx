17e5a4e688b12524ed6e9f4884acc462a4fac946
Naz <nazeefsuleman@gmail.com>
2023-02-13 08:02:57 -0500

    Added basic CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <div className="contacts-list">
+      <h1>My Contacts</h1>
+      <form action="" method="get">
+        <input type="text" placeholder='Search' />
+      </form>
+      <ul>
+        <li>nazeef Suleman</li>
+        <li>Zen Fae</li>
+        <li>amy</li>
+      </ul>
+     </div>
     </div>
   );
 }
